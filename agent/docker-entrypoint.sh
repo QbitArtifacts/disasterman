08868ee8fbe20146a2509b2e21a482b7f2c297ae
Lluis Santos <lluis@ngynx.com>
2019-03-13 16:15:36 +0100

    set first copy before set cron

--- agent/docker-entrypoint.sh
+++ agent/docker-entrypoint.sh
@@ -5,6 +5,9 @@ if [[ "$SERVER" == "" ]];then
     export SERVER=server
 fi
 
+#Do the first copy now
+/usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no" -a --delete /volumes $SERVER:/backups/latest/`hostname`
+
 envsubst < /backups.cron > /etc/cron.d/disasterman
 
 cron -f
