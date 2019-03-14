3fcd69a7ad88f2183afe46d413944470f1b2f404
Lluis Santos <lluis@ngynx.com>
2019-03-14 19:50:26 +0100

    :bug: fix, added full path to hostname

--- agent/copy_all_volumes_except_untagged.sh
+++ agent/copy_all_volumes_except_untagged.sh
@@ -9,5 +9,5 @@ ls /volumes | grep -E "^[a-f0-9]{64}$" | while read volume;do
     echo "/volumes/$volume" >> $EXCLUDEFILE
 done
 
-/usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no" -az --delete --exclude-from=$EXCLUDEFILE /volumes $SERVER:/backups/latest/`hostname`
+/usr/bin/rsync -e "ssh -o StrictHostKeyChecking=no" -az --delete --exclude-from=$EXCLUDEFILE /volumes $SERVER:/backups/latest/`/bin/hostname`
 
